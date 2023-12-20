534997b841c759b44ccf355589aa59c738939283
Plan Geranía <zahirasuarezporras@alumno.ieselrincon.es>
2023-12-20 20:04:18 +0000

    commit main

--- Assets/Scripts/PlayGame.cs
+++ Assets/Scripts/PlayGame.cs
@@ -9,5 +9,6 @@ public class PlayGame : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Return)) SCManager.instance.LoadScene("Game");
         print("HOLA");
+        print("HOLA");
     }
 }
